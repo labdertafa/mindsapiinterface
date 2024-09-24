8a1070b49bb7e3985b8558f08fdeef5f50c7e703
labderafa <ellaboratorioderafa@gmail.com>
2024-09-24 09:41:09 +0200

    Se agregaron métodos a la clase MindsAccount

--- src/main/java/com/laboratorio/mindsapiinterface/model/MindsAccount.java
+++ src/main/java/com/laboratorio/mindsapiinterface/model/MindsAccount.java
@@ -1,6 +1,8 @@
 package com.laboratorio.mindsapiinterface.model;
 
+import com.laboratorio.mindsapiinterface.utils.MindsApiConfig;
 import java.util.List;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,7 @@ import lombok.Setter;
  * @author Rafael
  * @version 1.0
  * @created 18/09/2024
- * @updated 23/09/2024
+ * @updated 24/09/2024
  */
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
@@ -91,4 +93,56 @@ public class MindsAccount {
     private List<MindsCarousel> carousels;
     private boolean blocked;
     private boolean blocked_by;
+    
+    public boolean isSeguidorPotencial() {
+        if ((this.blocked) || (this.blocked_by)) {
+            return false;
+        }
+        
+        if (this.subscriptions_count < 2) {
+            return false;
+        }
+
+        return 2 * this.subscriptions_count >= this.subscribers_count;
+    }
+    
+    public boolean isFuenteSeguidores() {
+        MindsApiConfig config = MindsApiConfig.getInstance();
+        int umbral = Integer.parseInt(config.getProperty("umbral_fuente_seguidores"));
+        return this.subscribers_count >= umbral;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + Objects.hashCode(this.guid);
+        hash = 61 * hash + Objects.hashCode(this.username);
+        hash = 61 * hash + Objects.hashCode(this.did);
+        hash = 61 * hash + Objects.hashCode(this.urn);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MindsAccount other = (MindsAccount) obj;
+        if (!Objects.equals(this.guid, other.guid)) {
+            return false;
+        }
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.did, other.did)) {
+            return false;
+        }
+        return Objects.equals(this.urn, other.urn);
+    }
 }
